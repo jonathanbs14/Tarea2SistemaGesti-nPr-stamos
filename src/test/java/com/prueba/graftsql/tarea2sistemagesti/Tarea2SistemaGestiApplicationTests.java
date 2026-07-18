bdc686b3d53267bc75a5d36a4b0b10b21cddb8fd
jonathanbs14 <146000888+jonathanbs14@users.noreply.github.com>
2026-07-17 20:23:35 -0500

    comit 1

---
+++ src/test/java/com/prueba/graftsql/tarea2sistemagesti/Tarea2SistemaGestiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.graftsql.tarea2sistemagesti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea2SistemaGestiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
